92ee44239b856b080f5bc37ff5c1e431fd0e041c
Don Myers <donmyers@me.com>
2022-10-18 13:30:40 -0400

    Dispatcher now expects output put class as responds from controller if it is a string it will append it to the output also added the type hinting

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace dmyers\orange;
 
+use dmyers\orange\Input;
+use dmyers\orange\Config;
+use dmyers\orange\Output;
 use dmyers\orange\exceptions\MethodNotFound;
 use dmyers\orange\exceptions\ControllerClassNotFound;
 
@@ -12,14 +15,14 @@ class Dispatcher
 	protected $input = null;
 	protected $output = null;
 
-	public function __construct(input &$input, output &$output, config &$config)
+	public function __construct(Input &$input, Output &$output, Config &$config)
 	{
 		$this->input = $input;
 		$this->output = $output;
 		$this->config = $config;
 	}
 
-	public function call(array $route): ?string
+	public function call(array $route): Output
 	{
 		$controllerClass = $route['controller'];
 
@@ -30,14 +33,20 @@ class Dispatcher
 				/* we found something */
 				$matches = array_map(function ($value) {
 					return urldecode($value);
-				}, $route['args']);
+				}, $route['argv']);
 
-				return (new $controllerClass($this->input, $this->output, $this->config))->$method(...$matches);
+				$output = (new $controllerClass($this->input, $this->output, $this->config))->$method(...$matches);
+
+				if (is_string($output)) {
+					$this->output->appendOutput($output);
+				}
 			} else {
 				throw new MethodNotFound($method);
 			}
 		} else {
 			throw new ControllerClassNotFound($controllerClass);
 		}
+
+		return $this->output;
 	}
 } /* end class */
