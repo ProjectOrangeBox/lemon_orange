4485d4ef621eae7af6bdcedb82281bcdff204611
Don Myers <donmyers@me.com>
2022-10-19 09:38:30 -0400

    pass log level by const (integer)

--- src/Log.php
+++ src/Log.php
@@ -43,9 +43,12 @@ class Log
 		'INFO'      => 64,
 		'DEBUG'     => 128,
 	];
+	protected $psrLevelsInt = [];
 
 	public function __construct(array $config)
 	{
+		$this->psrLevelsInt = array_flip($this->psrLevels);
+
 		/* defaults */
 		$this->config['filepath'] = __ROOT__ . '/var/logs/' . date('Y-m-d') . '-log.txt';
 
@@ -111,43 +114,51 @@ class Log
 		return $this->config['threshold'];
 	}
 
-	public function isEnabled(): Bool
+	public function isEnabled(): bool
 	{
 		return $this->enabled;
 	}
 
-	public function writeLog($level, $msg)
+	protected function convertLogLevelToString(int $level): string
 	{
-		if ($this->enabled) {
-			/* normalize */
-			$level = strtoupper($level);
-
-			/* does this log level even exist? */
-			/* bitwise PSR 3 Mode */
-			if (array_key_exists($level, $this->psrLevels) && $this->config['threshold'] & $this->psrLevels[$level]) {
-				$this->handler->$level($msg);
-			}
+		if (!isset($this->psrLevelsInt[$level])) {
+			throw new InvalidValue('Unknown Log PSR Level: ' . (string) $level);
 		}
+
+		return $this->psrLevelsInt[$level];
+	}
+
+	protected function convertLogLevelToInt(string $level): int
+	{
+		$level = strtoupper($level);
+
+		if (!isset($this->psrLevels[$level])) {
+			throw new InvalidValue('Unknown Log PSR Level: ' . (string) $level);
+		}
+
+		return $this->psrLevels[$level];
 	}
 
 	public function __call($name, $arguments)
 	{
-		$level = strtoupper($name);
-
-		if (!array_key_exists($level, $this->psrLevels)) {
-			throw new InvalidValue($name);
-		}
-
-		$this->internalWrite($level, $arguments[0]);
+		$this->writeLog($this->convertLogLevelToInt($name), $arguments[0]);
 	}
 
-	protected function internalWrite(string $level, string $msg)
+	public function writeLog(int $level, string $message): void
+	{
+		/* bitwise PSR 3 Mode */
+		if ($this->enabled && $this->config['threshold'] & $level) {
+			$this->internalWrite($level, $message);
+		}
+	}
+
+	protected function internalWrite(int $level, string $message): void
 	{
 		$write = '';
-		$isNew = false;
+		$isNewFile = false;
 
 		if (!file_exists($this->config['filepath'])) {
-			$isNew = true;
+			$isNewFile = true;
 
 			/* Only add protection to php files */
 			if ($this->isPhpFile) {
@@ -156,11 +167,12 @@ class Log
 		}
 
 		/* closure */
-		$write .= ($this->lineFormatter)($level, $msg);
+		$write .= ($this->lineFormatter)($this->convertLogLevelToString($level), $message);
 
+		/* Not atomic but we need append */
 		file_put_contents($this->config['filepath'], $write, FILE_APPEND | LOCK_EX);
 
-		if ($isNew) {
+		if ($isNewFile) {
 			chmod($this->config['filepath'], $this->config['permissions']);
 		}
 	}
