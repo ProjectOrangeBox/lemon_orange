c03017609c6215a9e19687832f45db9bf0bd53a8
Don Myers <donmyers@me.com>
2022-10-19 09:37:43 -0400

    add class use names

--- src/Controller.php
+++ src/Controller.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace dmyers\orange;
 
+use dmyers\orange\Input;
+use dmyers\orange\Config;
+use dmyers\orange\Output;
+
 class Controller
 {
 	protected $output = null;
 	protected $input = null;
 	protected $config = [];
 
-	public function __construct(input $input, output $output, config $config)
+	public function __construct(Input &$input, Output &$output, Config &$config)
 	{
 		$this->input = $input;
 		$this->output = $output;
